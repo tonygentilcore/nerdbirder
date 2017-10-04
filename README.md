2d67f81830da09d2607090b3001ad6e1fac98412
Tony Gentilcore <tonygentilcore@gmail.com>
2017-10-04 16:56:10 -0500

    Add mechanics for scraping nerdbirder instagram posts. Unused currently.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Usage
 
 ```sh
+pip install -r requirements.txt
 ./scripts/taxonomy.py  # generate json
 cd site
 python -m SimpleHTTPServer 8000
--- scripts/taxonomy.py
+++ scripts/taxonomy.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-'''Prints extant avian taxonomy.
+'''Generates json for taxonomical visualization of nerdbirder instagram photos.
 
 Usage: ./taxonomy.py
 '''
 
+import instagram_scraper
 import json
 import os
 from xml.dom import minidom
 
 IOC_NAMES = os.path.join(os.path.dirname(__file__), os.pardir, 'data', 'master_ioc-names_xml.xml')
 JSON_OUT = os.path.join(os.path.dirname(__file__), os.pardir, 'site', 'birds.json')
+INSTAGRAM_ACCOUNT = 'nerdbirder'
 
 def getChildrenByTagName(node, tag_name):
     rc = []
@@ -36,68 +38,104 @@ def getName(node):
 def isExtinct(node):
     return node.hasAttribute('extinct') and node.attributes['extinct'].value == 'yes'
 
-num_orders = 0
-num_families = 0
-num_genera = 0
-num_species = 0
-aves_json = {
-  'name': 'aves',
-  'children': []
-}
-
-list = minidom.parse(IOC_NAMES).getElementsByTagName('list')[0]
-orders = list.getElementsByTagName('order')
-for order in orders:
-    if isExtinct(order):
-        continue
-    num_orders += 1
-    order_name = getName(order)
-    order_json = {
-      'name': order_name,
+def getTaxonomyDict():
+    num_orders = 0
+    num_families = 0
+    num_genera = 0
+    num_species = 0
+    aves_json = {
+      'name': 'aves',
       'children': []
     }
-    aves_json['children'].append(order_json)
-    print(order_name)
 
-    families = order.getElementsByTagName('family')
-    for family in families:
-        if isExtinct(family):
+    list = minidom.parse(IOC_NAMES).getElementsByTagName('list')[0]
+    orders = list.getElementsByTagName('order')
+    for order in orders:
+        if isExtinct(order):
             continue
-        num_families += 1
-        family_name = getName(family)
-        family_json = {
-          'name': family_name,
+        num_orders += 1
+        order_name = getName(order)
+        order_json = {
+          'name': order_name,
           'children': []
         }
-        order_json['children'].append(family_json)
-        print('\t%s' % family_name)
+        aves_json['children'].append(order_json)
+        # print(order_name)
 
-        genera = family.getElementsByTagName('genus')
-        for genus in genera:
-            if isExtinct(genus):
+        families = order.getElementsByTagName('family')
+        for family in families:
+            if isExtinct(family):
                 continue
-            num_genera += 1
-            genus_name = getName(genus)
-            genus_json = {
-              'name': genus_name,
+            num_families += 1
+            family_name = getName(family)
+            family_json = {
+              'name': family_name,
               'children': []
             }
-            family_json['children'].append(genus_json)
-            print('\t\t%s' % genus_name)
+            order_json['children'].append(family_json)
+            # print('\t%s' % family_name)
 
-            speciess = genus.getElementsByTagName('species')
-            for species in speciess:
-                if isExtinct(species):
+            genera = family.getElementsByTagName('genus')
+            for genus in genera:
+                if isExtinct(genus):
                     continue
-                num_species += 1
-                species_name = getName(species)
-                genus_json['children'].append({
-                  'name': species_name,
-                  'size': 1000
-                })
-                print('\t\t\t%s' % species_name)
+                num_genera += 1
+                genus_name = getName(genus)
+                genus_json = {
+                  'name': genus_name,
+                  'children': []
+                }
+                family_json['children'].append(genus_json)
+                # print('\t\t%s' % genus_name)
 
-print('\n%d orders, %d families, %d genera, %d species' % (num_orders, num_families, num_genera, num_species))
+                speciess = genus.getElementsByTagName('species')
+                for species in speciess:
+                    if isExtinct(species):
+                        continue
+                    num_species += 1
+                    species_name = getName(species)
+                    genus_json['children'].append({
+                      'name': species_name,
+                      'size': 1000
+                    })
+                    # print('\t\t\t%s' % species_name)
 
-with open(JSON_OUT, 'w') as f:
-    f.write(json.dumps(aves_json))
+    print('\n%d orders, %d families, %d genera, %d species\n' % (num_orders, num_families, num_genera, num_species))
+
+    return aves_json
+
+def getFirstHashtag(caption):
+    return caption.split('#')[1].split()[0]
+
+def getInstagramDict():
+    result = {
+      'photos': []
+    }
+    instagram = instagram_scraper.InstagramScraper()
+    posts = instagram.media_gen(INSTAGRAM_ACCOUNT)
+    num_posts = 0
+    for post in posts:
+        caption = post['caption']['text']
+        images = post['images']
+        likes = post['likes']['count']
+        result['photos'].append({
+          'english_name': getFirstHashtag(caption),
+          'likes': likes,
+          'images': images
+        })
+        num_posts += 1
+        # print('%s %d' % (getFirstHashtag(caption), likes))
+
+    print('%d instagram photos' % num_posts)
+
+    return result
+
+def main():
+    instagram_dict = getInstagramDict()
+    taxonomy_dict = getTaxonomyDict()
+
+    with open(JSON_OUT, 'w') as f:
+        f.write(json.dumps(taxonomy_dict))
+
+if __name__ == '__main__':
+    main()
