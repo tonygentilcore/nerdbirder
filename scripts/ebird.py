9c3aecb66f9206a7c5824962aac8c7184224ab7e
Tony Gentilcore <tonygentilcore@gmail.com>
2017-10-25 13:00:07 -0700

    Create a json of genus -> english family name

---
+++ scripts/ebird.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+'''Generates json genus -> english family name for ebird checklist (clements).
+
+Usage: ./ebird.py
+'''
+
+import csv
+import json
+import os
+
+TAXONOMY = os.path.join(os.path.dirname(__file__), os.pardir, 'data', 'eBird_Taxonomy_v2017_18Aug2017.csv')
+JSON_OUT = os.path.join(os.path.dirname(__file__), os.pardir, 'site', 'families.json')
+
+genus_to_family = {}
+
+with open(TAXONOMY, 'rU') as csvfile:
+    rows = csv.DictReader(csvfile)
+    for row in rows:
+        if row['CATEGORY'] != 'species':
+            continue
+        genus, species = row['SCI_NAME'].split(' ')
+        _, english_name = row['FAMILY'].split('(')
+        genus_to_family[genus] = english_name[:-1].replace(' and Allies', ' et al').replace(', and ', ' & ').replace(' and ', ' & ')
+
+with open(JSON_OUT, 'w') as f:
+    f.write(json.dumps(genus_to_family, separators=(',\n', ':')))
